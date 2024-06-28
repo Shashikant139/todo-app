b7a96e8b6f82b55e97edbbe13559ddf0be4a530a
shashikant139 <mailshashi534@gmail.com>
2024-06-28 18:35:29 +0530

    todo_App

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import TaskList from './components/TaskList'
+import CompletedTask from './components/CompletedTask'
+
+const App: React.FC = () => {
+  return (
+    <>
+    <div className="todo_app_section">
+
+      <div className="todo_app_container">
+
+        <div className="new_task_list task_list">
+          <h2>Task List</h2>
+        </div>
+        <TaskList />
+      </div>
+    </div>
+  
+
+<div className="Completed_task_list task_list">
+  <h2>Completed Task</h2>
+  <div>
+   <CompletedTask/>
+   </div>
+</div>
+</>
+  )
+}
+
+export default App
