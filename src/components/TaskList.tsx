1b9a9375f5d35d4624b2112d7c042dee463b670a
shashikant139 <mailshashi534@gmail.com>
2024-06-29 18:10:56 +0530

    Task_List_Item created

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import "./TaskList.scss"
 
 const TaskList: React.FC = () => {
   return (
-    <div>TaskList</div>
+    <>
+    <div className="task_list_section">
+      <div className="task_list_item">
+        <input type="checkbox" title="check" className= "task_checkbox"/>
+        <input type="text" title="input_text" className="tet_input"/>
+        <button type="button" title="submit">Remove</button>
+      </div>
+    </div>
+    </>
   )
 }
 
