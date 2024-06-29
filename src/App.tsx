b7430233199e29a894d430903620c7fd80df24d6
shashikant139 <mailshashi534@gmail.com>
2024-06-29 17:23:01 +0530

    scss created

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,37 @@
 import React from 'react'
 import TaskList from './components/TaskList'
 import CompletedTask from './components/CompletedTask'
+import './App.scss'
 
-const App: React.FC = () => {
+const App:React.FC = () => {
   return (
     <>
-    <div className="todo_app_section">
+    <div className='todo_app_section'>
 
-      <div className="todo_app_container">
+      <div className='todo_app_container'>
 
-        <div className="new_task_list task_list">
+        <div className='new_task_list task_list'>
           <h2>Task List</h2>
+          
+        <div>
+          <TaskList/>
         </div>
-        <TaskList />
       </div>
-    </div>
-  
 
-<div className="Completed_task_list task_list">
-  <h2>Completed Task</h2>
-  <div>
-   <CompletedTask/>
-   </div>
+
+      <div>
+        <div className='completed_task_list task_list'>
+          <h2>Completed Task</h2>
+          </div>
+        <div>
+          <CompletedTask/>
+        </div>
+      </div>
+    
+      </div>
+      
 </div>
-</>
+    </>
   )
 }
 
